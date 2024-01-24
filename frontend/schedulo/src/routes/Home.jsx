d37b921a72f9c55f1d3beb2729eabf6142526cbd
Elisariane <elisarianeBS@gmail.com>
2024-01-24 14:13:40 -0300

    feat: Add list events

--- frontend/schedulo/src/routes/Home.jsx
+++ frontend/schedulo/src/routes/Home.jsx
@@ -1,31 +1,68 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Button, Card, Col, Container, Row, Spinner } from "react-bootstrap";
+import apiEventFecth from "../axios/config";
 
-const Home = () => {
-    
-        const URL = 'http://localhost:3000/events';
+const Home = () => { 
 
-        const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState([]);
+
+  const getEvents = async () => {
+    try {
+      const response = await apiEventFecth.get("/events");
+      const eventsData = response.data;
+
+      setEvents(eventsData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  return (
+    <>
+      {events.length === 0 ? (
+        <Container className="mt-4 text-center">
+            <Spinner  animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+            </Spinner>
+        </Container>
         
-        const getEvents = async() => {
-            try {
-                const response = await axios.get(URL);
-
-                setEvents(response);
-                console.log(response);
-            } catch (error) {
-                console.error(error)
-            }
-        };
-
-        useEffect(() => {
-            getEvents();
-        }, []);
-
-
-    return (
-        <div>Home</div>
-    );
-}
+      ) : (
+        events.map((event) => (
+            <>
+            <Container fluid>
+                <Row className="text-center">
+                    <Col sm>
+                    <div className="card-apresentation m-2" key={event.id}>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="https://images.unsplash.com/photo-1519750157634-b6d493a0f77c?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  />
+                  <Card.Body>
+                    <Card.Title>{event.title}</Card.Title>
+                    <Card.Text>{event.description}</Card.Text>
+                    <Button className="btn-sm m-2" variant="primary">
+                      Editar
+                    </Button>
+                    <Button className="btn-sm m-2" variant="danger">
+                      Excluir
+                    </Button>
+                  </Card.Body>
+                </Card>
+            </div>
+                    </Col>
+                </Row>
+            </Container>
+            </>
+        ))
+      )}
+    </>
+  );
+};
 
 export default Home;
--- frontend/schedulo/src/routes/NewEvent.jsx
+++ frontend/schedulo/src/routes/NewEvent.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import apiEventFecth from "../axios/config";
 
 const NewEvent = () => {
-    return (
-        <div>NewEvent</div>
-    );
-}
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [startTime, setStartTime] = useState();
+  const [endTime, setEndTime] = useState();
+
+  const createEvent = async (e) => {
+    e.preventDefault();
+    const event = {title, description, startTime, endTime, userId: 'e4ecb18c-bf19-4690-bca9-740bbd961893'};
+    
+    await apiEventFecth.post("/events", {
+        body: event,
+    });
+
+    navigate('/');
+
+};
+  return (
+    <>
+      <Container className="mt-4">
+        <h2 className="text-center">Adicione um novo Evento!</h2>
+        <Form onSubmit={(e) => createEvent(e)}>
+          <Form.Group className="mb-3">
+            <Form.Label>Título do evento</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Título legal"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Descrição</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              placeholder="Uma descrição bacana!"
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Data e Hora Inicio</Form.Label>
+            <Form.Control type="datetime-local"  onChange={(e) => setStartTime(e.target.value)}/>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Data e Hora Fim</Form.Label>
+            <Form.Control type="datetime-local"  onChange={(e) => setEndTime(e.target.value)}/>
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Adicionar
+          </Button>
+        </Form>
+      </Container>
+    </>
+  );
+};
 
 export default NewEvent;
