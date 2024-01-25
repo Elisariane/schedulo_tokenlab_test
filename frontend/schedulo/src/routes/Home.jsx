2a734cc4c6951c5a5b13c3d6c265af75961123dd
Elisariane <elisarianeBS@gmail.com>
2024-01-25 18:16:17 -0300

    feat: Remove buttons for non owner of events

--- frontend/schedulo/src/routes/Home.jsx
+++ frontend/schedulo/src/routes/Home.jsx
@@ -6,7 +6,7 @@ import "../index.css";
 import NavBar from "../components/Navbar";
 
 const Home = () => {
-  const [events, setEvents] = useState([]); 
+  const [events, setEvents] = useState([]);
 
   const getEvents = async () => {
     try {
@@ -19,41 +19,41 @@ const Home = () => {
     }
   };
 
-
   const deleteEvent = async (id) => {
     try {
       await apiEventFecth.delete(`/events/${id}`);
-      window.location.replace('/');
+      window.location.replace("/");
     } catch (error) {
       console.error(error);
     }
-  }
-
-  useEffect(() => {
-    getEvents();  
-  }, []);
-
-
-  const options = {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
   };
 
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  const options = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
 
   return (
     <>
-    <NavBar />
-    {events.length === 0 ? (
+      <NavBar />
+      {events.length === 0 ? (
         <Container className="mt-4 text-center">
           <span>Poxa ainda não teve nenhum evento cadastrado :(</span>
         </Container>
       ) : (
         events.map((event) => (
           <>
-            <div className="cards m-2 justify-content-center text-center" key={event.id}>
+            <div
+              className="cards m-2 justify-content-center text-center"
+              key={event.id}
+            >
               <Card style={{ width: "30rem" }}>
                 <Card.Img
                   variant="top"
@@ -62,20 +62,45 @@ const Home = () => {
                 <Card.Body>
                   <Card.Title>{event.title}</Card.Title>
                   <div className="container-hour ">
-                    <p className="text-hour">{new Intl.DateTimeFormat('pt-BR', options).format(new Date(event.startTime))}</p> -
-                    <p className="text-hour">{new Intl.DateTimeFormat('pt-BR', options).format(new Date(event.endTime))}</p>
+                    <p className="text-hour">
+                      {new Intl.DateTimeFormat("pt-BR", options).format(
+                        new Date(event.startTime)
+                      )}
+                    </p>{" "}
+                    -
+                    <p className="text-hour">
+                      {new Intl.DateTimeFormat("pt-BR", options).format(
+                        new Date(event.endTime)
+                      )}
+                    </p>
                   </div>
-                    
+
                   <Card.Text>{event.description}</Card.Text>
-                  
-                  <Link to={sessionStorage.getItem("userId") != event.userId ? '#' : `/update-event/${event.id}`}>
-                  <Button className="btn-sm m-2" variant="primary" disabled={sessionStorage.getItem("userId") != event.userId}   >
-                    Editar
-                  </Button>
-                  </Link>
-                  <Button className="btn-sm m-2" variant="danger" onClick={() => deleteEvent(event.id)} disabled={sessionStorage.getItem("userId") != event.userId} >
-                    Excluir
-                  </Button>
+
+                  {sessionStorage.getItem("userId") == event.userId ? (
+                    <div>
+                      <Link
+                        to={
+                          sessionStorage.getItem("userId") != event.userId
+                            ? "#"
+                            : `/update-event/${event.id}`
+                        }
+                      >
+                        <Button className="btn-sm m-2" variant="primary">
+                          Editar
+                        </Button>
+                      </Link>
+                      <Button
+                        className="btn-sm m-2"
+                        variant="danger"
+                        onClick={() => deleteEvent(event.id)}
+                      >
+                        Excluir
+                      </Button>
+                    </div>
+                  ) : (
+                    ""
+                  )}
                 </Card.Body>
               </Card>
             </div>
